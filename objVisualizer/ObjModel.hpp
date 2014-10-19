ad2b2afd3f9f1fa5cfd3d997c1ae56b63ad2d2e3
Simone Gasparini <simone.gasparini@gmail.com>
2014-10-19 14:30:38 +0200

    license header

--- objVisualizer/ObjModel.hpp
+++ objVisualizer/ObjModel.hpp
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /***************************************************************************
   Simple Class to load and draw 3D objects from OBJ files
   Using triangles and normals as static object. No texture mapping. 

de615886d711686cbce669b424c4b6f55e558ad4
Simone Gasparini <simone.gasparini@gmail.com>
2014-10-22 13:21:52 +0200

    added DEPRECATED macro (to be tested on older version gcc)

--- objVisualizer/core.hpp
+++ objVisualizer/core.hpp
@@ -17,6 +17,15 @@
 #include <GL/gl.h>
 #endif
 
+#ifdef __GNUC__
+#define DEPRECATED(func) func __attribute__ ((deprecated))
+#elif defined(_MSC_VER)
+#define DEPRECATED(func) __declspec(deprecated) func
+#else
+#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+#define DEPRECATED(func) func
+#endif //__attribute__ ((deprecated))
+
 #include <vector>
 #include <map>
 #include <iostream>
